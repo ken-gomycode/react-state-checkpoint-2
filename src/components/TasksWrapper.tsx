bd085da3188f864834fd8488e7ca93a859b76509
Ken Ovienadu <kenovienadu@gmail.com>
2025-04-30 00:46:31 +0100

    feat: refactor TasksWrapper to use useTaskList hook and improve empty state handling

--- src/components/TasksWrapper.tsx
+++ src/components/TasksWrapper.tsx
@@ -1,67 +1,27 @@
-import {useCallback, useMemo, useState} from "react";
-import {mockTasks} from "../data/mock.ts";
+import {useMemo} from "react";
 import TaskRow from "./TaskRow.tsx";
 import ActionButton from "./ActionButton.tsx";
-import {Task} from "../types";
+import useTaskList from "../hooks/useTaskList.ts";
+import TasksEmptyState from "./TasksEmptyState.tsx";
+import {getTodaysDate} from "../utils/dates.ts";
 
 const TasksWrapper = () => {
-  const [tasks, setTasks] = useState<Task[]>(mockTasks);
+  const {
+    tasks,
+    toggleTaskCompleted,
+    deleteTask,
+  } = useTaskList();
 
-  const sortedTasks = useMemo(() => {
-    return tasks.sort((a, b) => {
-      if (a.completed && !b.completed) return 1;
-      if (!a.completed && b.completed) return -1;
-      return 0;
-    });
-  }, [tasks]);
+  const readableDate = useMemo((): string => getTodaysDate(), []);
 
-  const readableDate = useMemo((): string => {
-    return new Date().toLocaleDateString("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "2-digit",
-      year: "numeric",
-    })
-  }, []);
-
-  const toggleTaskCompleted = useCallback((id: string) => {
-    // toggle task completed based on id
-    const updatedTasks = tasks.map(task => {
-      if (task.id === id) {
-        return {...task, completed: !task.completed};
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  }, [tasks]);
-
-  const deleteTask = useCallback((id: string) => {
-    // delete task based on id
-    const updatedTasks = tasks.filter(task => task.id !== id);
-    setTasks(updatedTasks);
-  }, [tasks]);
-
-  const renderEmptyState = useCallback(() => {
-    return (
-      <div className="flex flex-col items-center justify-center h-full pt-20">
-        <p className="text-gray-500 text-lg">No tasks available</p>
-        <p className="text-gray-400">Click the button above to add a task</p>
-      </div>
-    );
-  }, []);
-
-  const renderTasks = useCallback(() => {
-    if (!sortedTasks.length) return null;
-
-    return sortedTasks.map((task, index) => (
-      <TaskRow
-        key={index}
-        task={task}
-        toggleCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-      />
-    ));
-  }, [sortedTasks, toggleTaskCompleted, deleteTask]);
+  const taskRows = tasks.map((task, index) => (
+    <TaskRow
+      key={index}
+      task={task}
+      toggleCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
 
   return (
     <section className="bg-white w-2/5 p-8 rounded" >
@@ -75,7 +35,7 @@ const TasksWrapper = () => {
       </div>
 
       <div className="mt-8 min-h-[400px] max-h-[80vh] overflow-y-scroll">
-        {sortedTasks.length ? renderTasks() : renderEmptyState()}
+        {tasks.length ? taskRows : <TasksEmptyState />}
       </div>
     </section>
   );
